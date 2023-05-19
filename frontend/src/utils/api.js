c7c3f4ccf122136a53c51e19fa7a7f2465f3f410
Dmitry Botyan <dbotyan@list.ru>
2023-05-19 19:30:27 +0300

    настройка и деплой бэкенда

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,5 +1,5 @@
 import { configApi } from "./constants";
-const BASE_URL = "https://api.mestoo.students.nomoredomains.monster"
+const BASE_URL = "https://api.mestoo.students.nomoredomains.monster";
 
 class Api {
   constructor({ headers }, url) {
